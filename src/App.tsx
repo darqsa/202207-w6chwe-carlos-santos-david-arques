aac9d24e2f8943266a4aab5c7cd2030a4eb0795e
Carlos <casantosmu@gmail.com>
2022-08-14 11:07:50 +0200

    Added loadRobots to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
-const App = () => <h1>Hola Mundo</h1>;
+import { useSelector } from "react-redux";
+import { RootState } from "./app/store";
+import useApi from "./features/robots/hooks/useApi";
+
+const App = () => {
+  const robots = useSelector((state: RootState) => state.robots);
+  const { useLoadRobots } = useApi();
+  useLoadRobots();
+
+  return (
+    <div>
+      {robots.map((robot) => (
+        <h2 key={robot.name}>{robot.name}</h2>
+      ))}
+    </div>
+  );
+};
 
 export default App;
--- src/components/RobotCard/RobotCard.tsx
+++ src/components/RobotCard/RobotCard.tsx
@@ -34,4 +34,5 @@ const RobotCard = (): JSX.Element => {
     </RobotContainerStyled>
   );
 };
+
 export default RobotCard;
