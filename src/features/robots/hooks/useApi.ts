8f15477b449a3efeb739c075bd186d9b64d414ba
Carlos <casantosmu@gmail.com>
2022-08-17 00:05:05 +0200

    Added loginUser customHook && Added button to login

--- src/features/robots/hooks/useApi.ts
+++ src/features/robots/hooks/useApi.ts
@@ -5,6 +5,7 @@ import fetchApi from "../../../services/fetchApi";
 import { loadRobotsAction } from "../slices/robotsSlice";
 
 const robotsApiUrl = process.env.REACT_APP_ROBOTS_API_URL;
+const usersApiUrl = process.env.REACT_APP_USERS_API_URL;
 
 const useApi = () => {
   const dispatch = useDispatch();
@@ -16,7 +17,23 @@ const useApi = () => {
     dispatch(loadRobotsAction(robots));
   }, [dispatch]);
 
-  return { robots, loadRobots };
+  const loginUser = async (payload: { id: string; userName: string }) => {
+    const response = await fetch(`${usersApiUrl}/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    const {
+      user: { token },
+    } = await response.json();
+
+    localStorage.setItem("token", token);
+  };
+
+  return { robots, loadRobots, loginUser };
 };
 
 export default useApi;
